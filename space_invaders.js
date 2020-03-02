aaed18f5a255a8e93e430fa5d76b74d3cd54b25d
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-03-02 12:05:27 -0600

    blocks now move back and forth but the conditionals are set to specific blocks. Need to set so it any block touches either side blocks change direction.

--- space_invaders.js
+++ space_invaders.js
@@ -1,28 +1,25 @@
-let canvas = document.getElementById('canvas')
-let context = canvas.getContext('2d')
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 let invader;
 let invaders =[];
-console.log(invaders)
+console.log(invaders);
 let default_x = 20;
-let default_y = 20
-let resources = [
-    {name: 'cat', type:'image', src:'cat.jpg'},
-setInterval(updateGame, 500)
+let default_y = 20;
+let direction = -20;
 
-]
-
+setInterval(updateGame, 500);
 
 function startGame() {
 // create new game pieces and push to the invaders list
     for (let i = 0; i < 4; i++) {
-        default_y = 20
-        invader = new invaderBlock(60, 30, default_x, default_y)
-        invaders.push(invader)
-        default_x += 90
+        default_y = 20;
+        invader = new invaderBlock(60, 30, default_x, default_y);
+        invaders.push(invader);
+        default_x += 90;
 
         for (let i = 0; i < 4; i++) {
-            invader = new invaderBlock(60, 30, default_x, default_y)
-            invaders.push(invader)
+            invader = new invaderBlock(60, 30, default_x, default_y);
+            invaders.push(invader);
             default_y += 50
         }
     }
@@ -36,18 +33,27 @@ function invaderBlock(width, height, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
+
     this.update = function() {
-        context.fillStyle = 'black'
+        context.fillStyle = 'black';
         context.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 function updateGame() {
     // at each interval clear the canvas and add to invader.y; update through invaderBlock method on line 19
+    //TODO everything moves back and forth but I need to sort out how to make it if any black hits either side
     clearCanvas();
+        if(invaders[0].x <=5 || invaders[4].x >= 155){
+            direction *= -1;
+            invaders.forEach(function (element){
+                element.y += 5
+            })
+        }
         invaders.forEach(function (element) {
-            element.x -= 3;
-            element.update()
+                element.x += direction;
+                element.update()
+
         })
 }
 
